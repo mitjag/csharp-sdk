8fd52b146ba399527f2cfe22ac0903f173f823f5
Mitja Gomboc <mitjagomboc@yahoo.com>
2019-04-02 22:31:38 +0200

    websocket streams info

--- src/websocket/WebSocketClient.cs
+++ src/websocket/WebSocketClient.cs
@@ -6,5 +6,20 @@ namespace binance.dex.sdk.websocket
 {
     public class WebSocketClient
     {
+        /*
+         * WebSocket Streams:
+         * Orders
+         * Account
+         * Transfer
+         * Trades
+         * Diff. Depth Stream
+         * Book Depth Streams
+         * Kline/Candlestick Streams
+         * Individual Symbol Ticker Streams
+         * All Symbols Ticker Streams
+         * Individual Symbol Mini Ticker Streams
+         * All Symbols Mini Ticker Streams
+         * Blockheight
+         */
     }
 }
