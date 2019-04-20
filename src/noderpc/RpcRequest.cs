6c659c4c5925b30151d218cb097f26a5bc40afae
Mitja Gomboc <mitjagomboc@yahoo.com>
2019-04-20 22:24:02 +0200

    node rpc classes

---
+++ src/noderpc/RpcRequest.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace binance.dex.sdk.noderpc
+{
+    public class RpcRequest
+    {
+        /*
+            {
+                "method": "broadcast_tx_sync",
+                "jsonrpc": "2.0",
+                "params": [
+                "abc"
+                ],
+                "id": "dontcare"
+            }
+        */
+
+        [JsonProperty("method")]
+        private string Method { get; set; }
+
+        [JsonProperty("jsonrpc")]
+        private string jsonrpc { get; set; }
+
+        [JsonProperty("params")]
+        private List<string> Params { get; set; }
+
+        [JsonProperty("id")]
+        private string Id { get; set; }
+    }
+}
